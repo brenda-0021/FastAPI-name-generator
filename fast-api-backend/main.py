1c5deb96d2f69a4529d9c72b175db9ebfffebc58
Brenda Z <brenda.zamarripa5773@alumnos.udg.mx>
2024-10-21 19:08:14 -0600

    cambios 13

--- fast-api-backend/main.py
+++ fast-api-backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-from utils import classes  # Asegúrate de que esta importación sea correcta
+from utils.classes import APIException, RandomNameGenerator
 
 app = FastAPI()
 
@@ -17,11 +17,11 @@ app.add_middleware(
 @app.get("/generate", status_code=200)
 def generate_random_name(response: Response, nationality: str = "nepal", country: str = "nepal", gender: str = "male", count: int = 5):
     try:
-        random_name_generator = classes.RandomNameGenerator(nationality, country, gender)
+        random_name_generator = RandomNameGenerator(nationality, country, gender)
         
         # Generar múltiples nombres
-        random_names = random_name_generator.make_api_call(count)  # Ajustar según tu método
-        return {"random_names": random_names}  # Cambia aquí para devolver múltiples nombres
-    except classes.APIException as err:
+        random_names = random_name_generator.make_api_call(count)
+        return {"random_names": random_names}
+    except APIException as err:
         response.status_code = err.status_code
         return {"err_message": str(err)}
